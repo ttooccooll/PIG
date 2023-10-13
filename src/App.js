770d3cde38890ff4cf1f5076513a1d7d017ab646
Jason <jasonbohio@gmail.com>
2023-10-13 13:25:15 -0400

    initial

--- src/App.js
+++ src/App.js
@@ -126,187 +126,38 @@ function App() {
   return (
     <div className="App">
       <header >
-        <h1 > BTC-1701-D -- United Federation of Sovereign Individuals </h1>
+        <h1 >  </h1>
       </header>
       <div>
        <AudioPlayerComponent autoplay={true} />
       </div>
-      <div className="row">
-        <div className="button-holder-holder">
-          <div className="button-holder">
-            <Buttons />
+      <div className="pigpic">
+        <div className="row">
+          <div className="button-holder-holder">
+            <div className="button-holder">
+              <Buttons />
+            </div>
           </div>
         </div>
-        <div className="button-next">
-          <div className="insidebutton-next"></div>
-        </div>
-        <div className="button-nextest">
-          <div className="insidebutton-nextest"></div>
-        </div>
-        <div className="button-nextestly">
-          <div className="insidebutton-nextestly"></div>
-        </div>
-        <div className="button-nextiest">
-          <div className="insidebutton-nextiest">
-            <p>
-              <a 
-              className="p"
-              href="https://mempool.space/"
-              onClick={playMP3}
-              target="_blank"
-              rel="noopener noreferrer">
-                MEM.SPC
-              </a>
-              <a 
-              className="p"
-              href="https://www.pleblab.com/"
-              onClick={playMP3}
-              target="_blank"
-              rel="noopener noreferrer">
-                P.Lab
-              </a>
-              <a 
-              className="p"
-              href="https://www.st-minutiae.com/resources/rulesofacquisition.html"
-              onClick={playMP3}
-              target="_blank"
-              rel="noopener noreferrer">
-                R0A
-              </a>
-              <a 
-              className="p"
-              href="https://bitfeed.live/"
-              onClick={playMP3}
-              target="_blank"
-              rel="noopener noreferrer">
-                BIT.LIV
-              </a>
-              <div>
-              <WhitePaper />
-              </div>
-              <a 
-              className="p"
-              href="https://lnvisualizer.com/"
-              onClick={playMP3}
-              target="_blank"
-              rel="noopener noreferrer">
-                LN-VIS
-              </a>
-              <a 
-              className="p"
-              href="https://bitcoinexplorer.org/"
-              onClick={playMP3}
-              target="_blank"
-              rel="noopener noreferrer"
-              class="element"non-breaking-word>
-                BTC-EXP
-              </a>
-              <a 
-              className="p"
-              href="https://www.youtube.com/watch?v=sZt6eU5REN8"
-              onClick={playMP3}
-              target="_blank"
-              rel="noopener noreferrer"
-              color="#D45F10">
-                12-25-2364
-              </a>
-              <div>
-              <PdfModal />
-              </div>
-              <a 
-              className="p"
-              href="https://nakamotoinstitute.org/cypherpunk-manifesto/"
-              onClick={playMP3}
-              target="_blank"
-              rel="noopener noreferrer"
-              color="#D45F10">
-                03-09-93
-              </a>
-              <a 
-              className="p"
-              href="https://www.youtube.com/watch?v=YRyioZK6BOc"
-              onClick={playMP3}
-              target="_blank"
-              rel="noopener noreferrer"
-              color="#D45F10">
-                5.P0T
-              </a>
-              <a 
-              className="p"
-              href="https://1ml.com/statistics"
-              onClick={playMP3}
-              target="_blank"
-              rel="noopener noreferrer"
-              color="#D45F10">
-                LN.1ML
-              </a>
-            </p>
+        <div className="row">
+          <div className="balance-card">
+            <div className="balance-content">
+              <h2>Sats</h2>
+              <p>{balance}</p>
+            </div>
+          </div>
+          <div className="balance-card">
+            <div className="balance-content">
+                <h2>USD-BTC</h2>
+                <p>{price}</p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="row">
-        <div className="balance-card">
-          <div className="balance-content">
-            <h2>Satoshis</h2>
-            <p>{balance}</p>
-          </div>
-        </div>
-        <div className="balance-card">
-          <div className="balance-content">
-              <h2>USD-BTC</h2>
-              <p>{price}</p>
-          </div>
-        </div>
-      </div>
-      <div className="row">
-        <div className="row-item">
-          <div className="row-itemer">
-            <Transactions transactions={transactions} />
-          </div>
-        </div>
-        <div className="row-itemz">
-          <Chart chartData={chartData} />
-        </div>
-      </div>
-      <div className="mostprefooter">
-        <div className="inside-footer">
-         <div class="content-container">
-          <ul>
-            {chartData &&
-              chartData.map((dataPoint, index) => (
-                <li key={index}>
-                  {new Date(dataPoint.x).toLocaleString('en-US', { hour12: false }).replace(/\//g, '.')}, USD-BTC: {dataPoint.y}
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div className="moreprefooter">
-        <div className="inside-footer">
-          <div class="content-container">
-            <ul>
-              {chartData &&
-                chartData.map((dataPoint, index) => (
-                  <li key={index}>
-                    Stardate {new Date(dataPoint.x).toLocaleString('en-US', { hour12: false }).replace(/\//g, '.')}, USD-BTC: {dataPoint.y}
-                  </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div className="prefooter">
-        <div className="inside-footer">
-          <div class="content-container">
-            <ul>
-              {chartData &&
-                chartData.map((dataPoint, index) => (
-                  <li key={index}>
-                    Stardate {new Date(dataPoint.x).toLocaleString('en-US', { hour12: false }).replace(/\//g, '.')}, USD-BTC: {dataPoint.y}
-                  </li>
-                ))}
-            </ul>
+        <div className="row">
+          <div className="row-item">
+            <div className="row-itemer">
+              <Transactions transactions={transactions} />
+            </div>
           </div>
         </div>
       </div>
